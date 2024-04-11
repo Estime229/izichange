33d73c4dcfd1a71f5c43aec13ea6be07284faa04
Estime229 <estimegliti108@gmail.com>
2024-04-11 11:59:17 +0100

    Exercices 1 et 2

---
+++ app/Models/Product.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    
+    protected $fillable = ['name', 'price_ht', 'creation_date', 'date_update'];
+    protected $casts = [
+        'creation_date' => 'datetime',
+        'date_update' => 'datetime',
+    ];
+}
---
+++ database/migrations/2024_04_11_103646_create_products_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->string('name', 255);
+            $table->float('price_ht');
+            $table->dateTime('creation_date');
+            $table->dateTime('date_update')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
